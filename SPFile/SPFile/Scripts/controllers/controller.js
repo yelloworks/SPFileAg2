cb1fb4656960893834e436718d4ee6cf24900b36
Alexander Rogov <guardienn@gmail.com>
2016-11-16 21:13:56 +0300

    Step1

--- SPFile/SPFile/Scripts/controllers/controller.js
+++ SPFile/SPFile/Scripts/controllers/controller.js
@@ -103,4 +103,42 @@
     init();
 
 }]);
+angular.module('Abs').controller('DropdownCtrl', function ($scope, $log) {
+    $scope.items = [
+      'The first choice!',
+      'And another choice for you.',
+      'but wait! A third!'
+    ];
 
+    $scope.status = {
+        isopen: false
+    };
+
+    $scope.toggled = function (open) {
+        $log.log('Dropdown is now: ', open);
+    };
+
+    $scope.toggleDropdown = function ($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+        $scope.status.isopen = !$scope.status.isopen;
+    };
+
+    $scope.appendToEl = angular.element(document.querySelector('#dropdown-long-content'));
+});
+angular.module('Abs').controller('TabsDemoCtrl', function ($scope, $window) {
+    $scope.tabs = [
+      { title: 'Dynamic Title 1', content: 'Dynamic content 1' },
+      { title: 'Dynamic Title 2', content: 'Dynamic content 2', disabled: true }
+    ];
+
+    $scope.alertMe = function () {
+        setTimeout(function () {
+            $window.alert('You\'ve selected the alert tab!');
+        });
+    };
+
+    $scope.model = {
+        name: 'Tabs'
+    };
+});
