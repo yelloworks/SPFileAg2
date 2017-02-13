cb6c5c337404bc38a4a98ad9b332e20d61db52ac
Alexander Rogov <guardienn@gmail.com>
2017-02-13 22:31:01 +0300

    Paste methods added. Not Integrated

--- SPFile/SPFile/Scripts/controllers/controller.js
+++ SPFile/SPFile/Scripts/controllers/controller.js
@@ -2,8 +2,8 @@
 
     angular.module('Abs', ['ngAnimate', 'ngSanitize', 'ui.bootstrap', 'TreeWidget', 'ngTouch', 'ui.grid', 'ngSelectable']);
 
-
-    angular.module('Abs')
+//TreeCtrl not neded
+   /* angular.module('Abs')
         .controller('TreeController',
         [
             '$scope', function($scope) {
@@ -149,6 +149,158 @@
 
             }
         ]);
+*/
+
+
+    //on page load
+    angular.element(document)
+        .ready(function() {
+            var $inj = angular.bootstrap(document.body, ['Abs']);
+            var $rootScope = $inj.get("$rootScope");
+
+            function getParameterByName(name, url) {
+                if (!url) {
+                    url = window.location.href;
+                }
+                name = name.replace(/[\[\]]/g, "\\$&");
+                var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+                    results = regex.exec(url);
+                if (!results) return null;
+                if (!results[2]) return '';
+                return decodeURIComponent(results[2].replace(/\+/g, " "));
+            }
+
+            SP.SOD.executeFunc('SP.Runtime.js',
+                'SP.ClientContext',
+                function() {
+                    SP.SOD.executeFunc('SP.js',
+                        'SP.ClientContext',
+                        function() {
+                            var ListId = GetUrlKeyValue("SPListId");
+
+
+                            var ItemId = GetUrlKeyValue("SPItemId");
+                            var ItemUrl = GetUrlKeyValue("SPItemUrl");
+                            var SiteUrl = GetUrlKeyValue("SPSiteUrl");
+                            var ListUrlDir = GetUrlKeyValue("SPListUrlDir");
+                            var Source = GetUrlKeyValue('SPSource', false);
+                            var SelectedListId = GetUrlKeyValue("SPSelectedListId");
+                            var SelectedItemId = GetUrlKeyValue("SPSelectedItemId");
+                            var HostUrl = GetUrlKeyValue("SPHostUrl");
+                            var Title = GetUrlKeyValue("SPTitle");
+                            var someTmp = GetUrlKeyValue("SPAppWebUrl");
+
+
+                            var decodeSourse = decodeURIComponent(Source);
+                            var rootFolder = getParameterByName('RootFolder', decodeSourse);
+                            var AppWebUrl2 = getParameterByName('SPSource');
+                            var folderName = GetUrlKeyValue('SPListUrlDir');
+                            //var url = window.location.protocol +
+                            //    "//" +
+                            //    window.location.host +
+                            //    _spPageContextInfo.siteServerRelativeUrl;
+
+                            var relativeUrl = folderName;
+                            if (rootFolder != null) {
+                                relativeUrl = rootFolder;
+                            }
+                            
+
+                            //Вынести в метод что ли но потом. Плохой код
+
+                            var it = someTmp.split('/');
+                            var itnew = it.slice(0, it.length-1);
+                            var url = itnew.join('/');
+
+                            if (relativeUrl != "/") {
+                                var folderArray = relativeUrl.split('/');
+                                folderName = folderArray.slice(folderArray.length - 1, folderArray.length - 0)[0];
+                            } 
+
+                            if (ListId != "") {
+                                $rootScope.$broadcast("pageInit", url, ListId, relativeUrl, folderName);
+                                $rootScope.$digest();
+                            }
+                        });
+                });
+        });
+
+
+    angular.module('Abs')
+        .controller('RibbonButtonsCtrl',
+            ['$scope', 'selectedBufferService', 'bufferCtrl', function($scope,selectedBufferService,bufferCtrl ) {
+
+                $scope.copyBtnClicked = function () {
+                    bufferCtrl.setBuffer(selectedBufferService.getBuffer());
+                };
+                $scope.pasteBtnClicked = function() {
+
+                };
+                $scope.cutBtnClicked = function() { 
+
+                };
+
+
+                function copyOrMoveFile(url, fileId, sourceLibrary, destLibrary, moveFile) {
+                    var ctx = new SP.ClientContext(url);
+                    var web = ctx.get_web();
+                    var currentLib = web.get_lists().getById(sourceLibrary);
+                    //getListTitle(url, sourceLibrary);
+                    var item = currentLib.getItemById(fileId);
+                    var file = item.get_file();
+                    ctx.load(currentLib);
+                    ctx.load(file);
+                    ctx.executeQueryAsync(
+                        function (sender, args) {
+                            if (file != null) {
+                                // var webUrl = ctx.get_web().get_serverRelativeUrl();
+                                var title = currentLib.get_title();
+                                var destFileUrl = url + '/' + destLibrary + '/' + file.get_name();
+                                moveFile
+                                    ? file.moveTo(destFileUrl, SP.MoveOperations.overwrite)
+                                    : file.copyTo(destFileUrl, SP.MoveOperations.overwrite);    ////для идиота, тут копирование выше перемещение
+
+                                ctx.executeQueryAsync(
+                                function (sender, args) {
+                                    alert("File copied.");
+                                },
+                                    function (sender, args) {
+                                        alert("Error copying the file.");
+                                    }
+                                 );
+                            }
+                            else {
+                                alert("404 FILE NOT FOUND!!!");
+                            }
+                        },
+                        function (sender, args) {
+                            alert("Error while getting the file.");
+                        }
+                    );
+                }
+
+
+                function getListTitle(url, listId) {
+                    var currentcontext = new SP.ClientContext(url);
+                    var currentweb = currentcontext.get_web();
+                    var listCollection = currentweb.get_lists().getById(listId);;
+                    currentcontext.load(listCollection);
+                    currentcontext.executeQueryAsync(function (sender, args) {
+                        //var listEnumerator = listCollection.getEnumerator();
+                        //var list = listEnumerator.get_current();
+                        var title = listCollection.get_title();
+                    },
+                        function (sender, args) {
+
+                        });
+                };
+
+
+
+
+
+
+            }]);
 
     angular.module('Abs')
         .controller('DropdownCtrl',
@@ -167,61 +319,6 @@
                 $scope.appendToEl = angular.element(document.querySelector('#dropdown-long-content'));
             });
 
-    angular.element(document).ready(function () {
-        var $inj = angular.bootstrap(document.body, ['Abs']);
-        var $rootScope = $inj.get("$rootScope");
-
-        function getParameterByName(name, url) {
-            if (!url) {
-                url = window.location.href;
-            }
-            name = name.replace(/[\[\]]/g, "\\$&");
-            var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-                results = regex.exec(url);
-            if (!results) return null;
-            if (!results[2]) return '';
-            return decodeURIComponent(results[2].replace(/\+/g, " "));
-        }
-
-        SP.SOD.executeFunc('SP.Runtime.js',
-            'SP.ClientContext',
-            function() {
-                SP.SOD.executeFunc('SP.js',
-                    'SP.ClientContext',
-                    function() {
-                        var ListId = GetUrlKeyValue("SPListId");
-
-
-                        var ItemId = GetUrlKeyValue("SPItemId");
-                        var ItemUrl = GetUrlKeyValue("SPItemUrl");
-                        var SiteUrl = GetUrlKeyValue("SPSiteUrl");
-                        var ListUrlDir = GetUrlKeyValue("SPListUrlDir");
-                        var Source = GetUrlKeyValue('SPSource', false);
-                        var SelectedListId = GetUrlKeyValue("SPSelectedListId");
-                        var SelectedItemId = GetUrlKeyValue("SPSelectedItemId");
-                        var HostUrl = GetUrlKeyValue("SPHostUrl");
-                        var Title = GetUrlKeyValue("SPTitle");
-                        //var AppWebUrl = GeturlKeyValue('SPAppWebUrl');
-
-                        var decodeSourse = decodeURIComponent(Source);
-                        var SiteUrl2 = getParameterByName('RootFolder', decodeSourse);
-                        var AppWebUrl2 = getParameterByName('SPSource');
-
-                        var url = window.location.protocol +
-                            "//" +
-                            window.location.host +
-                            _spPageContextInfo.siteServerRelativeUrl;
-
-                        var relativeUrl = "/";
-
-                        if (ListId != "") {
-                            $rootScope.$broadcast("pageInit", url, ListId, relativeUrl);
-                            $rootScope.$digest();
-                        }
-                    });
-            }); 
-    });
-   
 
     angular.module('Abs')
         .controller('tabsCtrl',
@@ -246,9 +343,9 @@
                     $scope.$broadcast('selectedEvent', $index);
                 };
 
-                var addNewWorkspace = function (url, listId, relativeUrl) {
+                var addNewWorkspace = function (url, listId, relativeUrl, folderName) {
                     $scope.tabs.push({
-                        title: listId,
+                        title: folderName,
                         url: url,
                         listId: listId,
                         relativeUrl: relativeUrl
@@ -258,22 +355,24 @@
                     });
                 };
 
+                $scope.addWorkspace = function () {
 
-                $scope.addWorkspace = function() {
                     var url = window.location.protocol +
                         "//" +
                         window.location.host +
                         _spPageContextInfo.siteServerRelativeUrl;
-                    addNewWorkspace(url, '{38405EBF-043B-4CE5-9440-744C20169CC0}', "/");
+
+                    addNewWorkspace(url, "","", "root");
                 };
+
                 $scope.removeTab = function (index, event) {
                     event.preventDefault();
                     $scope.tabs.splice(index, 1);
                 };
 
 
-                $scope.$on('pageInit', function (event, url, listId, relativeUrl) {
-                    addNewWorkspace(url, listId, relativeUrl);
+                $scope.$on('pageInit', function (event, url, listId, relativeUrl, folderName) {
+                    addNewWorkspace(url, listId, relativeUrl, folderName);
                 });
 
 
@@ -363,7 +462,7 @@
                     } else {
                         $scope.fileItems = [];
                         $scope.tabs[$scope.index].listId = "";
-                        $scope.tabs[$scope.index].title = $scope.tabs[$scope.index].url;
+                        $scope.tabs[$scope.index].title = "root";
                         $scope.tabs[$scope.index].relativeUrl = "";
                         $scope.upBtndisabled = true;
                         $scope.getLibrariesOnly();
@@ -381,11 +480,18 @@
                 $scope.onStart = function($index) {
                     $scope.index = $index;
 
+                    var arrayPath = $scope.tabs[$scope.index].relativeUrl.split('/');
+                    if (arrayPath.length > 2) {
+                        $scope.getDir();
+                    } else {
+                        $scope.upBtndisabled = true;
+                        $scope.getLibrariesOnly();
+                    }
                     //var listId = $scope.tabs[$scope.index].title;
                     //var url = $scope.tabs[$scope.index].url;
                     //var relativeUrl = $scope.tabs[$scope.index].relativeUrl;
 
-                    $scope.getDir();
+
                 };
 
 
@@ -452,7 +558,8 @@
 
             }]);
 
-    angular.module('Abs')
+    //mainCtrl not needed
+    /*angular.module('Abs')
         .controller('MainCtrl',
         [
             '$scope', function ($scope) {
@@ -544,8 +651,11 @@
             }
         ]);
 
+    */
 
 
+
+    //Servisies
     angular.module('Abs')
         .factory('bufferCtrl',
             function() {
@@ -557,6 +667,11 @@
                     buffer = [];
                     buffer.push(item);
                 };
+                methods.setBuffer = function(item) {
+                    buffer = [];
+                    buffer = item;
+                }
+
                 methods.getFBuffer = function() {
                     return buffer;
                 };
@@ -577,7 +692,7 @@
                 selectedPathID = bufferPathID;
                 selectedItemsBuffer.push(bufferItems);
             };
-            methods.getFBuffer = function () {
+            methods.getBuffer = function () {
                 return selectedItemsBuffer;
             };
             methods.getPathID = function () {
